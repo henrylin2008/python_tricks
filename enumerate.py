b892e799436fca6c7703edc7db3b7ea46473af80
hlin <henrylin2008@yahoo.com>
2020-01-23 21:28:43 -0800

    enumerate

--- enumerate.py
+++ enumerate.py
@@ -1,2 +1,10 @@
 # https://www.youtube.com/watch?v=C-gEQdGVXbk&t=561s
-names = ['Corey', ]
+names = ['Corey', 'Chris', 'Dave', 'Travis']
+
+# index = 0    #Beginner's code
+# for name in names:
+#     print(index, name)
+#     index += 1
+
+for index, name in enumerate(names, start=1): # index starts at 1
+    print(index, name)
