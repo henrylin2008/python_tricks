9d8d68531e92bcb570a38f1f6d23b5d856ce350a
hlin <henrylin2008@yahoo.com>
2020-01-23 21:24:42 -0800

    updated version

--- context_managers.py
+++ context_managers.py
@@ -1,6 +1,13 @@
-f = open('test.txt', 'r')
-file_contents = f.read()
-f.close()
+# f = open('test.txt', 'r')
+# file_contents = f.read()
+# f.close()
+#
+# words = file_contents.split(' ')
+# word_count = len(words)
+# print(word_count)
+
+with open('test.txt', 'r') as f:
+    file_contents = f.read()
 
 words = file_contents.split(' ')
 word_count = len(words)
