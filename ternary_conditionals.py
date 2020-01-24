500e2640710d98e06b3f521f9c8cd15040290d43
hlin <henrylin2008@yahoo.com>
2020-01-23 21:16:23 -0800

    conditionals, underscore palcehodlers

---
+++ ternary_conditionals.py
@@ -0,0 +1,14 @@
+# condition = True
+#
+# if condition:
+#     x = 1
+# else:
+#     x = 0
+#
+# print(x)
+
+# short version (Trick):
+condition = True
+
+x = 1 if condition else 0
+print(x)
---
+++ underscore_placeholders.py
@@ -0,0 +1,10 @@
+num1 = 100000000000
+num2 = 100000000
+#
+# total = num1 + num2
+# print(total)
+
+# num1 = 10_000_000_000
+# num2 = 100_000_000
+total = num1 + num2
+print(f'{total:,}') # string formatting, , in between 0 
