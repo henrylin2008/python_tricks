f3c25e9fe7013046ccf0108d3db6f147d7a78609
hlin <henrylin2008@yahoo.com>
2020-01-23 21:38:46 -0800

    unpacking tuple

--- zip.py
+++ zip.py
@@ -1,6 +1,13 @@
 names = ['Peter Parker', 'Clark Kent', 'Wade Wilson', 'Bruce Wayne']
 heroes = ['Spiderman', 'Superman', 'Deadpool', 'Batman']
+universes = ['Marvel', 'DC', 'Marvel', 'DC']
 
-for index, name in enumerate(names):
-    hero = heroes[index]
-    print(f'{name} is actually {hero}')
+# for index, name in enumerate(names):
+#     hero = heroes[index]
+#     print(f'{name} is actually {hero}')
+
+for name, hero, universe in zip(names, heroes, universes):  #
+    print(f'{name} is actually {hero} from {universe}')
+
+for value in zip(names, heroes, universes): # print values of 3 tuples
+    print(value)
