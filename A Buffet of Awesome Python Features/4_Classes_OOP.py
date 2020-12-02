9884b485576689de74666d8e173206a59fe11cab
hlin <henrylin2008@yahoo.com>
2020-12-02 09:23:51 -0800

    define custom exception classes

--- A Buffet of Awesome Python Features/4_Classes_OOP.py
+++ A Buffet of Awesome Python Features/4_Classes_OOP.py
@@ -32,13 +32,15 @@ my_car
 str(my_car)
 # '__str__ for Car'
 repr(my_car)
+
+
 # '__repr__ for Car'
 
 
 # The benefit is you won’t have to modify the __repr__ implementation when the class name changes.
 def __repr__(self):
-   return (f'{self.__class__.__name__}('
-           f'{self.color!r}, {self.mileage!r})')
+    return (f'{self.__class__.__name__}('
+            f'{self.color!r}, {self.mileage!r})')
 
 
 repr(my_car)
@@ -46,10 +48,55 @@ repr(my_car)
 print(my_car)
 # 'Car(red, 37281)'
 str(my_car)
+
+
 # 'Car(red, 37281)'
 
 # Key Takeaways
 # -The result of __str__ should be readable. The result of __repr__ should be unambiguous.
 # -Always add a __repr__ to your classes. The default implementation for __str__ just calls __repr__.
 # -Use __unicode__ instead of __str__ in Python 2.
+
+# 4.3 Defining Your Own Exception Classes
+# Build meaningful custom exception (instead of built-in exception)
+# Easy for troubleshooting
+class NameTooShortError(ValueError):
+    pass
+
+
+def validate(name):
+    if len(name) < 10:
+        raise NameTooShortError(name)
+
+
+# Exception Hierarchy
+# declare a base class
+class BaseValidationError(ValueError):
+    pass
+
+# "Real" Error Classes derived from the base error class
+class NameTooShortError(BaseValidationError):
+    pass
+
+class NameTooLongError(BaseValidationError):
+    pass
+
+class NameTooCuteError(BaseValidationError):
+    pass
+
+# handle all of the errors
+# try:
+#     validate(name)
+# except BaseValidationError as err:
+#     handle_validation_error(err)
+
+# defining custom exception classes makes it easier for your users to adopt an it’s easier to ask for forgiveness
+# than permission (EAFP) coding style that’s considered more Pythonic.
+#
+# Key Takeaways:
+# -Defining your own exception types will state your code’s intent more clearly and make it easier to debug.
+# -Derive your custom exceptions from Python’s built-in Exception class or from more specific exception classes like
+# ValueError or KeyError.
+# -You can use inheritance to define logically grouped exception hierarchies.
+
 
