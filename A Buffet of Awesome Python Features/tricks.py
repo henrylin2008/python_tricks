63a99fe99c03963d86ab6f179508ae19176d25ed
hlin <henrylin2008@yahoo.com>
2020-12-12 15:11:15 -0800

    added comment

--- A Buffet of Awesome Python Features/tricks.py
+++ A Buffet of Awesome Python Features/tricks.py
@@ -2,7 +2,6 @@
 xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
 sorted(xs.items(), key=lambda x: x[1])
 # [('d', 1), ('c', 2), ('b', 3), ('a', 4)]
-
 # Or:
 import operator
 sorted(xs.items(), key=operator.itemgetter(1))
@@ -29,7 +28,7 @@ if any((x, y, z)):
 # How to merge two dictionaries in Python 3.5+
 x = {'a': 1, 'b': 2}
 y = {'b': 3, 'c': 4}
-z = {**x, **y}
+z = {**x, **y}  # duplicate item (b) from y overwrites same item in x
 z
 # {'c': 4, 'a': 1, 'b': 3}
 
