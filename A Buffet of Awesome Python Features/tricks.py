5f0bd8e35d05cd88a3bfaa751bbd7180ccf3b735
hlin <henrylin2008@yahoo.com>
2020-12-14 08:09:21 -0800

    dict get() method and default

--- A Buffet of Awesome Python Features/tricks.py
+++ A Buffet of Awesome Python Features/tricks.py
@@ -113,4 +113,21 @@ from collections import OrderedDict
 items = ["foo", "bar", "bar", "foo"]
 print(list(OrderedDict.fromkeys(items).keys()))  # ['foo', 'bar']
 
+# The get() method on dicts and its "default" argument
 
+name_for_userid = {
+    382: "Alice",
+    590: "Bob",
+    951: "Dilbert",
+}
+
+
+def greeting(userid):
+    return "Hi %s!" % name_for_userid.get(userid, "there")
+
+
+greeting(382)
+# "Hi Alice!"
+
+greeting(333333)
+# "Hi there!"
