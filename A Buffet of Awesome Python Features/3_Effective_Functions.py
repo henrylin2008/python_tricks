628b62c8c87cff62fc7fbea4443b0fbdaafb206f
hlin <henrylin2008@yahoo.com>
2020-12-15 08:08:14 -0800

    Python's functions key takeaways

--- A Buffet of Awesome Python Features/3_Effective_Functions.py
+++ A Buffet of Awesome Python Features/3_Effective_Functions.py
@@ -6,10 +6,58 @@
 def yell(text):
     return text.upper() + '!'
 
+
 bark = yell
 
 bark.__name__
 # 'yell'
 
 # Functions Can Be Stored in Data Structures
+#
+# Functions can be nested
+# Python allows functions to be defined inside other functions. These are often called nested functions or inner
+# functions.
+
+def get_speak_func(volume):
+    def whisper(text):
+        return text.lower() + '...'
+
+    def yell(text):
+        return text.upper() + '!'
+
+    if volume > 0.5:
+        return yell
+    else:
+        return whisper
+
+
+# get_speak_func doesn’t actually call any of its inner functions—it simply selects the appropriate inner function
+# based on the volume argument and then returns the function object:
+
+
+# Functions can capture local state
+# Functions that do this are called lexical closures (or just closures, for short). A closure remembers the values from
+# its enclosing lexical scope even when the program flow is no longer in that scope.
+#
+
+
+# Objects can behave like functions
+# If an object is callable it means you can use the round parentheses function call syntax on it and even pass in
+# function call arguments. This is all powered by the __call__ dunder method.
+#
+# there’s a built-in callable function to check whether an object appears to be callable or not:
+callable(plus_3)
+# True
+callable(yell)
+# True
+callable('hello')
+# False
+
+# Key Takeaways
+# -Everything in Python is an object, including functions. You can assign them to variables,
+#  store them in data structures, and pass or return them to and from other functions (first-class functions.)
+# -First-class functions allow you to abstract away and pass around behavior in your programs.
+# -Functions can be nested and they can capture and carry some of the parent function’s state with them. Functions that
+#  do this are called closures.
+# -Objects can be made callable. In many cases this allows you to treat them like functions.
 #
