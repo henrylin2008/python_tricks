abc58bb89b23daf28756d00bafcddfe63f8529fc
hlin <henrylin2008@yahoo.com>
2020-12-19 07:10:30 -0800

    json.dumps()

--- A Buffet of Awesome Python Features/tricks.py
+++ A Buffet of Awesome Python Features/tricks.py
@@ -173,3 +173,19 @@ print_vector(**dict_vec)
 # <1, 0, 1>
 print_vector(*dict_vec)  # unpacking the keys
 # <x, y, z>
+
+
+# Using json.dumps() to pretty-print dicts
+# The standard string repr for dicts is hard to read:
+my_mapping = {'a': 23, 'b': 42, 'c': 0xc0ffee}
+my_mapping
+# {'b': 42, 'c': 12648430. 'a': 23}  # 😞
+
+# The "json" module can do a much better job:
+import json
+print(json.dumps(my_mapping, indent=4, sort_keys=True))
+{
+    "a": 23,
+    "b": 42,
+    "c": 12648430
+}
