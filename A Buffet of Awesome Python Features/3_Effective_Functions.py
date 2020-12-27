4cdc0ebb9dbe36e2e52022324bb27fdaf71a0c38
hlin <henrylin2008@yahoo.com>
2020-12-26 23:16:17 -0800

    decorator example

--- A Buffet of Awesome Python Features/3_Effective_Functions.py
+++ A Buffet of Awesome Python Features/3_Effective_Functions.py
@@ -10,6 +10,8 @@ def yell(text):
 bark = yell
 
 bark.__name__
+
+
 # 'yell'
 
 # Functions Can Be Stored in Data Structures
@@ -64,7 +66,9 @@ callable('hello')
 
 # 3.2 Lambdas are single-expression functions
 tuples = [(1, 'd'), (2, 'b'), (4, 'a'), (3, 'c')]
-sorted(tuples, key=lambda x: x[1]) # sorting a list of tuples by the second value in each tuple
+sorted(tuples, key=lambda x: x[1])  # sorting a list of tuples by the second value in each tuple
+
+
 # [(4, 'a'), (2, 'b'), (3, 'c'), (1, 'd')]
 
 # Key Takeaways
@@ -78,6 +82,19 @@ sorted(tuples, key=lambda x: x[1]) # sorting a list of tuples by the second valu
 # And, they let you do that without permanently modifying the wrapped function itself. The function’s behavior changes
 # only when it’s decorated.
 # A decorator is a callable that takes a callable as input and returns another callable.
+def null_decorator(func):
+    return func
+
+
+@null_decorator
+def greet():
+    return 'Hello!'
+# Putting an @null_decorator line in front of the function definition is the same as defining the function first and
+# then running through the decorator.
+
+
+greet()
+# 'Hello!'
 
 # Key Takeaways:
 # -Decorators define reusable building blocks you can apply to a callable to modify its behavior
@@ -92,6 +109,3 @@ sorted(tuples, key=lambda x: x[1]) # sorting a list of tuples by the second valu
 #
 
 #
-
-
-
