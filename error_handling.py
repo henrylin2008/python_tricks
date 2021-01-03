b4eff7b8b5408550f91dfd7aaa10bcbe746cae18
hlin <henrylin2008@yahoo.com>
2021-01-03 10:33:02 -0800

    custom exceptions

--- error_handling.py
+++ error_handling.py
@@ -49,3 +49,61 @@ try:
     f = open("test.txt", encoding='utf-8')  # perform file operations
 finally:
     f.close()  # clean up the resource before the program comes to a halt whether it successfully ran or not
+
+
+# Creating Custom Exceptions
+# In Python, users can define custom exceptions by creating a new class. This exception
+# class has to be derived, either directly or indirectly, from the built-in Exception class.
+
+# When we are developing a large Python program, it is a good practice to place all the user-defined exceptions that
+# our program raises in a separate file. Many standard modules do this. They define their exceptions separately as
+# exceptions.py or errors.py (generally but not always).
+#
+# define Python user-defined exceptions
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class ValueTooSmallError(Error):
+    """Raised when the input value is too small"""
+    pass
+
+
+class ValueTooLargeError(Error):
+    """Raised when the input value is too large"""
+    pass
+
+
+# you need to guess this number
+number = 10
+
+# user guesses a number until he/she gets it right
+while True:
+    try:
+        i_num = int(input("Enter a number: "))
+        if i_num < number:
+            raise ValueTooSmallError
+        elif i_num > number:
+            raise ValueTooLargeError
+        break
+    except ValueTooSmallError:
+        print("This value is too small, try again!")
+        print()
+    except ValueTooLargeError:
+        print("This value is too large, try again!")
+        print()
+
+print("Congratulations! You guessed it correctly.")
+
+# Enter a number: 12
+# This value is too large, try again!
+#
+# Enter a number: 0
+# This value is too small, try again!
+#
+# Enter a number: 8
+# This value is too small, try again!
+#
+# Enter a number: 10
+# Congratulations! You guessed it correctly.
