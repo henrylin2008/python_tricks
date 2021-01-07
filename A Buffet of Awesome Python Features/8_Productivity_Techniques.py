963209f7594dafe60a083f9efc532db2da3117ac
hlin <henrylin2008@yahoo.com>
2021-01-07 06:50:48 -0800

    Productivity Techniques

---
+++ A Buffet of Awesome Python Features/8_Productivity_Techniques.py
@@ -0,0 +1,17 @@
+# 8.1 Exploring Python Modules and Objects
+# Key Takeaways
+# -Use the built-in dir() function to interactively explore Python modules and classes from an interpreter session.
+# -The help() built-in lets you browse through the documentation right from your interpreter (hit q to exit)
+
+# 8.2 Isolating Project Dependencies with Virtualenv
+# Key Takeaways:
+# -Virtual environments keep your project dependencies separated. They help you avoid version conflicts between packages
+#  and different versions of the Python runtime
+# -As a best practice, all of your Python projects should use virtual environments to store their dependencies. This
+#  will help avoid headaches.
+
+# 8.3 Peeking Behind the Bytecode Curtain
+# key Takeaways:
+# -CPython executes programs by first translating them into an intermediate bytecode and then running the bytecode on a
+#  stack-based virtual machine
+# -You can use the built-in dis module to peek behind the scenes and inspect the bytecode.
