3f16d7d76907ee2456ad8698d38e709d1a24aa80
hlin <henrylin2008@yahoo.com>
2021-01-11 06:14:34 -0800

    http.server

--- A Buffet of Awesome Python Features/PyTricks.py
+++ A Buffet of Awesome Python Features/PyTricks.py
@@ -442,3 +442,17 @@ print(cars)  # ['bmw', 'audi', 'toyota', 'subaru']
 print(sorted(cars))  # ['audi', 'bmw', 'subaru', 'toyota']
 print(cars)  # ['bmw', 'audi', 'toyota', 'subaru']
 sorted(cars, reverse=True)     # ['toyota', 'subaru', 'bmw', 'audi']
+
+
+# Python has a HTTP server built into the
+# standard library. This is super handy for
+# previewing websites.
+
+# Python 3.x
+$ python3 -m http.server
+
+# Python 2.x
+$ python -m SimpleHTTPServer 8000
+
+# (This will serve the current directory at
+#  http://localhost:8000)
