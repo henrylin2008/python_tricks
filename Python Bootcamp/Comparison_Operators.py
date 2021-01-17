c5aa4e0549bf09a4d7290afb4e5a2ee686528d0b
hlin <henrylin2008@yahoo.com>
2021-01-17 10:52:46 -0800

    Comparison Operators

---
+++ Python Bootcamp/Comparison_Operators.py
@@ -0,0 +1,38 @@
+# Table of Comparison Operators
+# Operator	Description	Example
+# ==	If the values of two operands are equal, then the condition becomes true.	(a == b) is not true.
+# !=	If values of two operands are not equal, then condition becomes true.	(a != b) is true
+# <>	If values of two operands are not equal, then condition becomes true.	(a <> b) is true. This is similar to != operator.
+# >	If the value of left operand is greater than the value of right operand, then condition becomes true.	(a > b) is not true.
+# <	If the value of left operand is less than the value of right operand, then condition becomes true.	(a < b) is true.
+# >=	If the value of left operand is greater than or equal to the value of right operand, then condition becomes true.	(a >= b) is not true.
+# <=	If the value of left operand is less than or equal to the value of right operand, then condition becomes true.	(a <= b) is true.
+
+# Equal
+2 == 2  # True
+1 == 0  # False
+
+
+# Not Equal
+2 != 1  # True
+2 != 2  # False
+2 <> 1  # True
+2 <> 2  # False
+
+
+# Greater Than
+2 > 1   # True
+2 > 4   # False
+
+# Less Than
+
+2 < 4   # True
+2 < 1   # False
+
+# Greater Than or Equal to
+2 >= 2  # True
+2 >= 1  # True
+
+# Less than or Equal to
+2 <= 2  # True
+2 <= 4  # True
