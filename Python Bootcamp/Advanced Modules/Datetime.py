f40c78f9babeda1ad7e1bd90880c0d4e0ee6739e
hlin <henrylin2008@yahoo.com>
2021-01-18 11:07:31 -0800

    date, datetime, timedelta

--- Python Bootcamp/Advanced Modules/Datetime.py
+++ Python Bootcamp/Advanced Modules/Datetime.py
@@ -51,7 +51,7 @@ print('Resolution:', datetime.date.resolution)  # Resolution: 1 day, 0:00:00
 
 # Another way to create new date instances uses the replace() method of an existing date. For example, you can change
 # the year, leaving the day and month alone
-d1 = datetime.date(2015, 3, 11)     # only the date (note including hours, minutes, seconds)
+d1 = datetime.date(2015, 3, 11)  # only the date (note including hours, minutes, seconds)
 print('d1:', d1)  # d1: 2015-03-11
 d2 = d1.replace(year=1990)  # update the year; also available (month, date)
 print('d2:', d2)  # d2: 1990-03-11
@@ -60,18 +60,92 @@ print('d2:', d2)  # d2: 1990-03-11
 # We can perform arithmetic on date objects to check for time differences. For example:
 d1  # datetime.date(2015, 3, 11)
 d2  # datetime.date(1990, 3, 11)
-d1 - d2     # datetime.timedelta(9131)
+d1 - d2  # datetime.timedelta(9131)
 # This give us the difference in days between the two dates. You can use the timedelta method to specify various
 # units of times (day,minutes,hours,etc...)
 result = d1 - d2
-print(result) # 9131 days, 0:00:00
-result.days     # 9131
+print(result)  # 9131 days, 0:00:00
+result.days  # 9131
 
-
-dt1 = datetime.datetime(2021, 11, 3, 22, 0)     # 2021-11-03 22:00:00
-dt2 = datetime.datetime(2020, 11, 3, 12, 0)     # 2020-11-03 12:00:00
-dt1 - dt2       # datetime.timedelta(days=365, seconds=36000)
-3600/60/60      # 10 hours
+dt1 = datetime.datetime(2021, 11, 3, 22, 0)  # 2021-11-03 22:00:00
+dt2 = datetime.datetime(2020, 11, 3, 12, 0)  # 2020-11-03 12:00:00
+dt1 - dt2  # datetime.timedelta(days=365, seconds=36000)
+3600 / 60 / 60  # 10 (hours)
 diff = dt1 - dt2
-diff.seconds    # 36000
-diff.total_seconds()    # 31572000.0; total seconds = 36000 seconds + 365 days in seconds
+diff.seconds  # 36000
+diff.total_seconds()  # 31572000.0; total seconds = 36000 seconds + 365 days in seconds
+
+# get Current Date and Time
+import datetime
+
+now = datetime.datetime.now()  # get current local date and time
+print(now)  # 2021-01-18 10:39:29.835696
+
+today = datetime.date.today()  # get current date
+print(today)  # 2021-10-18
+
+d = datetime.date(2020, 4, 13)  # set a date
+print(d)  # 2020-04-13
+
+# get current date
+from datetime import date
+
+# date object of today's date
+today = date.today()
+
+print("Current year:", today.year)  # 2021
+print("Current month:", today.month)  # 01
+print("Current day:", today.day)  # 18
+
+# time object to represent time
+from datetime import time
+
+# time(hour = 0, minute = 0, second = 0)
+a = time()
+print("a =", a)  # a = 00:00:00
+
+# time(hour, minute and second)
+b = time(11, 34, 56)
+print("b =", b)  # b = 11:34:56
+
+# time(hour, minute and second)
+c = time(hour=11, minute=34, second=56)
+print("c =", c)  # c = 11:34:56
+
+# time(hour, minute, second, microsecond)
+d = time(11, 34, 56, 234566)
+print("d =", d)  # d = 11:34:56.234566
+
+# difference between two dates and times
+from datetime import date, datetime
+
+t1 = date(2018, 7, 12)
+t2 = date(2017, 12, 23)
+t3 = t1 - t2
+print(t3)  # 201 days, 0:00:00
+
+t4 = datetime(2018, 7, 12, 7, 9, 33)
+t5 = datetime(2019, 6, 10, 5, 55, 13)
+print(t4 - t5)  # -333 days, 1:14:20
+
+# difference between two timedelta objects
+
+from datetime import timedelta
+
+t1 = timedelta(weeks=2, days=5, hours=1, seconds=33)
+t2 = timedelta(days=4, hours=11, minutes=4, seconds=54)
+t3 = t1 - t2
+
+print("t3 =", t3)  # t3 = 14 days, 13:55:39
+
+# negative timedelta object
+from datetime import timedelta
+
+t1 = timedelta(seconds=33)
+t2 = timedelta(seconds=54)
+t3 = t1 - t2
+print("t3 =", t3)  # t3 = -1 day, 23:59:39
+print("t3 =", abs(t3))  # t3 = 0:00:21
+
+t = timedelta(days=5, hours=1, seconds=33, microseconds=233423)
+print("total seconds =", t.total_seconds())  # total seconds = 435633.233423
