0da1c009c68f219580eefe9e1836a2f8d1d630bf
hlin <henrylin2008@yahoo.com>
2021-01-20 08:46:57 -0800

    Quantifiers, groups

--- Python Bootcamp/Advanced Modules/regular_expressions.py
+++ Python Bootcamp/Advanced Modules/regular_expressions.py
@@ -32,3 +32,26 @@ phone = re.search(r'\d\d\d-\d\d\d-\d\d\d\d', text)
 phone   # <re.Match object; span=(23, 35), match='408-555-1234'>
 phone.group()   # '408-555-1234'
 
+# Quantifiers
+# Now that we know the special character designations, we can use them along with quantifiers to define
+# how many we expect.
+# Character	Description         Example Pattern Code	Example Match
+# +	        1 or more times	    Version \w-\w+	        Version A-b1_1
+# {3}	    exactly 3 times	    \D{3}	                abc
+# {2,4}	    2 to 4 times	    \d{2,4}	                123
+# {3,}	    3 or more	        \w{3,}	                anycharacters
+# \*	    0 or more times	    A\*B\*C*	            AAACC
+# ?	        Once or none        plurals?	            plural
+phone = re.search(r'\d{3}-\d{3}-\d{4}', text)   # same as above
+# Groups
+# What if we wanted to do two tasks, find phone numbers, but also be able to quickly extract their area code (
+# the first three digits). We can use groups for any general task that involves grouping together regular expressions
+# (so that we can later break them down).
+#
+phone_pattern = re.compile(r'(\d{3})-(\d{3})-(\d{4})')  # group patterns
+results = re.search(phone_pattern, text)
+results.group()     # '408-555-1234'
+results.group(1)    # '408'
+results.group(2)    # '555'
+results.group(3)    # '1234'
+results.groups()    # ('408', '555', '1234')
