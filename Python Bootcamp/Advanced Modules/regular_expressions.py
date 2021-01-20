97c33478a8d5ab43b05ae179e0cae8a69c373a13
hlin <henrylin2008@yahoo.com>
2021-01-20 07:24:54 -0800

    re patterns

--- Python Bootcamp/Advanced Modules/regular_expressions.py
+++ Python Bootcamp/Advanced Modules/regular_expressions.py
@@ -18,3 +18,17 @@ len(matches)    # 2
 for match in re.finditer('phone', text):
     print(match.span())     # (3, 8), (18, 23)
     print(match.group())    # phone, phone
+
+# Patterns
+# Character	Description	        Example Pattern    Example Match
+# \d	    A digit	            file_\d\d          file_25
+# \w	    Alphanumeric	    \w-\w\w\w	       A-b_1
+# \s	    White space	        a\sb\sc	           a b c
+# \D	    A non digit	        \D\D\D	           ABC
+# \W	    Non-alphanumeric	\W\W\W\W\W	       *-+=)
+# \S	    Non-whitespace	    \S\S\S\S	       Yoyo
+text = "My telephone number is 408-555-1234"
+phone = re.search(r'\d\d\d-\d\d\d-\d\d\d\d', text)
+phone   # <re.Match object; span=(23, 35), match='408-555-1234'>
+phone.group()   # '408-555-1234'
+
