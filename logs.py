95c1bc2163f664cee5642f6b36f0da530fc2b98c
hlin <henrylin2008@yahoo.com>
2021-02-13 11:20:42 -0800

    set logger configs individually

--- logs.py
+++ logs.py
@@ -9,9 +9,39 @@ logging.basicConfig(filename="logs.log",
                     datefmt='%m/%d/%Y %H:%M:%S')
 # filemode='w': overwrite the log file
 
+# ---------------------------Alternative Logger config ----------------------- #
+# Alternatively: set filename, level, format, and file handler individually; modular
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+#
+# formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+#
+# file_handler = logging.FileHandler('sample.log')
+# file_handler.setLevel(logging.ERROR)
+# file_handler.setFormatter(formatter)
+#
+# stream_handler = logging.StreamHandler()
+# stream_handler.setFormatter(formatter)
+#
+# logger.addHandler(file_handler)
+# logger.addHandler(stream_handler)
+# ----------------------------------------------------------------------------- #
+
+
+# Ex: logged message with traceback
+def divide(x, y):
+    """Divide Function"""
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        logger.exception('Tried to divide by zero')  # log with traceback
+    else:
+        return result
+
+
 # basicConfig: default log level is WARNING = 30;
 # logger set to be >= (set level), ex: debug: 10 and above
-logger = logging.getLogger(__name__)    # __name__ = '__main__'
+logger = logging.getLogger(__name__)  # __name__ = '__main__'
 
 # Log level and the messages
 logger.debug("Debug message.")
