69d9808dcd39fe296d38397bb24bba3eda1d0d73
hlin <henrylin2008@yahoo.com>
2021-02-15 15:51:47 -0800

    send email with Sendgrid

--- sending_emails.py
+++ sending_emails.py
@@ -47,3 +47,26 @@ smtp_object.quit()  # Close the session
 #     if part.get_content_type() == 'text/plain':   # or 'text/html'
 #        body = part.get_payload(decode=True)
 #        print(body)
+
+
+# Send email with sendgrid
+# import os
+# import sendgrid
+# from sendgrid.helpers.mail import Content, Email, Mail
+#
+# sg = sendgrid.SendGridAPIClient(
+#     apikey=os.environ.get("SENDGRID_API_KEY")
+# )
+# from_email = Email("my@gmail.com")
+# to_email = Email("your@gmail.com")
+# subject = "A test email from Sendgrid"
+# content = Content(
+#     "text/plain", "Here's a test email sent through Python"
+# )
+# mail = Mail(from_email, subject, to_email, content)
+# response = sg.client.mail.send.post(request_body=mail.get())
+#
+# # The statements below can be included for debugging purposes
+# print(response.status_code)
+# print(response.body)
+# print(response.headers)
