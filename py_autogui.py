36679cee80a13f617d8ae4dc57fdaf4659f8ac91
hlin <henrylin2008@yahoo.com>
2021-02-17 08:39:31 -0800

    message box functions

--- py_autogui.py
+++ py_autogui.py
@@ -9,7 +9,7 @@ print(pyautogui.position())
 # Fail-Safes
 # pyautogui.PAUSE = 2.5   # set up a 2.5 second pause
 # pyautogui.FAILSAFE = True # moving the mouse to the upper-left will raise a pyautogui.FailSafeException that can
-                            # abort your program
+# abort your program
 
 # Mouse Functions
 # pyautogui.moveTo(348, 81, duration=1)  # move mouse to XY coordinates over num_second seconds
@@ -40,3 +40,12 @@ pyautogui.typewrite(['enter'])
 # pyautogui.screenshot("/Users/hlin/Downloads/screenshot1.png") # screenshot with the file name and the location
 # pyautogui.locateOnScreen("filename")  # look for picture (filename) on the screen; return (x, y, width, height)
 # pyautogui.locateCenterOnScreen("filename") # locate the center of file/image on the screen
+# pyautogui.locateAllOnScreen(): function will return a generator for all the locations it is found on the screen
+
+
+# Message Box functions
+pyautogui.alert(text='', title='', button='OK')
+pyautogui.confirm(text='', title='', buttons=['OK', 'Cancel'])  # text display on the screen; title of the window;
+pyautogui.prompt(text='text', title='title', default='sweet')  # 'little mango'; default = default text
+pyautogui.password(text='password', title='window', default='pw123', mask='*')
+# text: text display on the screen; title: window title; default password; mask: * as characters
