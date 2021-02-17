8f2825e647ba51a056796c7ae3a3bd487dbfbeac
hlin <henrylin2008@yahoo.com>
2021-02-16 23:04:28 -0800

    PyAutoGUI

---
+++ py_autogui.py
@@ -0,0 +1,33 @@
+import pyautogui
+
+# General Functions
+# move mouse over to the position, ctrl+shift+R to get the current mouse (x, y)
+print(pyautogui.position())
+# print(pyautogui.size())     # current screen resolution width and height
+# pyautogui.onScreen(x, y)  # True if x & y are within the screen.
+
+# Fail-Safes
+# pyautogui.PAUSE = 2.5   # set up a 2.5 second pause
+# pyautogui.FAILSAFE = True # moving the mouse to the upper-left will raise a pyautogui.FailSafeException that can
+                            # abort your program
+
+# Mouse Functions
+# pyautogui.moveTo(348, 81, duration=1)  # move mouse to XY coordinates over num_second seconds
+# pyautogui.moveTo(x, y, duration=num_seconds)  # move mouse to XY coordinates over num_second seconds
+# pyautogui.moveRel(xOffset, yOffset, duration=num_seconds)  # move mouse relative to its current position
+# pyautogui.dragTo(x, y, duration=num_seconds)  # drag mouse to XY
+# pyautogui.dragRel(xOffset, yOffset, duration=num_seconds)  # drag mouse relative to its current position
+pyautogui.click(348, 81, button='left')
+# pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
+
+# Keyboard Functions
+pyautogui.typewrite('Hello World!\n')
+# pyautogui.typewrite('Hello world!\n', interval=secs_between_keys)  # useful for entering text, newline is Enter
+pyautogui.typewrite(['enter'])
+# pyautogui.typewrite(['a', 'b', 'c', 'left', 'backspace', 'enter', 'f1'], interval=secs_between_keys)
+
+# pyautogui.hotkey("command", "space")
+# pyautogui.hotkey('ctrl', 'c')  # ctrl-c to copy
+# pyautogui.hotkey('ctrl', 'v')  # ctrl-v to paste
+# pyautogui.keyDown(key_name)   # Button down
+# pyautogui.keyUp(key_name)     # Button up
