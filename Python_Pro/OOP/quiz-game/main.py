3c121b275bc906610e694039004c6d195fb00c1d
hlin <henrylin2008@yahoo.com>
2021-03-01 07:33:06 -0800

    QuizBrain and if there's still questions

--- Python_Pro/OOP/quiz-game/main.py
+++ Python_Pro/OOP/quiz-game/main.py
@@ -1,7 +1,7 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
-# questions = Question(question, answer)
 question_bank = []
 for question in question_data:
     text = question["text"]
@@ -9,4 +9,8 @@ for question in question_data:
     new_question = Question(text, answer)
     question_bank.append(new_question)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
+while quiz.still_has_questions:
+    quiz.next_question()
