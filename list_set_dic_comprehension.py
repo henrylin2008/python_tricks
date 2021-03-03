552b4f369dfa6a6ac7e073ff56b16d34ac456fcf
hlin <henrylin2008@yahoo.com>
2021-03-03 08:45:07 -0800

    added dictionary comprehension

--- list_set_dic_comprehension.py
+++ list_set_dic_comprehension.py
@@ -22,3 +22,13 @@ new_word = {i for i in word}  # no repeat string
 print(new_word)               # {'t', 's', 'e', 'w'}
 
 
+# Dictionary Comprehension
+nums = [1, 2, 3, 4, 5]
+square_dict = {num: num**2 for num in nums}
+print(square_dict)            # {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+
+# Increase the price by 50% for any price > 2.0
+old_price = {"milk": 1.02, "coffee": 2.5, "bread": 2.5}
+new_price = {item: price*1.5 if price > 2 else price for (item, price) in old_price.items()}
+new_price           # {'milk': 1.02, 'coffee': 3.75, 'bread': 3.75}
