504018e18ee2562b68338c450af916378305a11a
hlin <henrylin2008@yahoo.com>
2021-12-01 16:02:08 -0800

    one-liners

--- one-liners.py
+++ one-liners.py
@@ -20,9 +20,9 @@ import random as r
 p = 'abcdefghijklmnopqrstuvwxyz0123456789%^*(-_=+)'
 print(''.join([p[r.randint(0, len(p) - 1)] for i in range(10)]))
 
-
 # Display the Current Date and Time in String Format
 import datetime
+
 print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 # Check if a String Is a Palindrome
@@ -34,3 +34,22 @@ print('Yes') if str1 == str1[::-1] else print('No')  # No
 print('Yes') if str2 == str2[::-1] else print('No')  # Yes
 print('Yes') if str3 == str3[::-1] else print('No')  # No
 print('Yes') if str4 == str4[::-1] else print('No')  # Yes
+
+# Find Factorial of a Number
+num1 = 5
+num2 = 0
+num3 = 10
+num4 = 12
+factorial = lambda num: 1 if num <= 1 else num * factorial(num - 1)
+print("Factorial of", num1, ":", factorial(num1))   # 120
+print("Factorial of", num2, ":", factorial(num2))   # 1
+print("Factorial of", num3, ":", factorial(num3))   # 3628800
+print("Factorial of", num4, ":", factorial(num4))   # 479001600
+
+
+# Fibonacci Sequence Upto N Terms
+from functools import reduce
+fibSequence = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]], range(n-2), [0, 1])
+print(fibSequence(10))      # [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+print(fibSequence(5))       # [0, 1, 1, 2, 3]
+print(fibSequence(6))       # [0, 1, 1, 2, 3, 5]
